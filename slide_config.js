0c67fb957ad3c611fe9fe27b7149695ac9f8ec2d
hpallaoro <hpallaoro@gmail.com>
2015-09-02 07:10:42 -0300

    Update slide_config.js

--- slide_config.js
+++ slide_config.js
@@ -1,14 +1,18 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'BSE Salud',
-    subtitle: 'Consultoría Tecnológica en Salud',
+    title: 'Title Goes Here<br>Up To Two Lines',
+    subtitle: 'Subtitle Goes Here',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/bse_salud_logo_tiny.png',
+    favIcon: 'images/google_developers_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -18,11 +22,11 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Hugo A. Pallaoro',
-    company: 'Responsable IT<br>BSE Salud',
-    gplus: 'https://ar.linkedin.com/in/hpallaoro',
-    twitter: '@hpallaoro',
-    www: 'http://www.bsesalud.com',
+    name: 'Firstname Lastname',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
     github: 'http://github.com/you'
   }/*, {
     name: 'Second Name',
